dce262623728f27ce219ff39679f128a9886683a
Anderneo <anderneo.nardi@gmail.com>
2022-07-25 09:04:14 -0300

    criado classe Usuario

---
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.eternos.magiadoslivros.domain.model;
+
+import java.util.Date;
+
+import com.eternos.magiadoslivros.domain.model.Perfil;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Usuario {
+    private String r;
+    private Genero genero;
+    private Perfil  perfil;
+    private Boolean doador;
+    private Date dataDeNas;
+    private Date dataDeCadastro;
+    
+
+    //incluirUsuario()  void
+    //atualizarUsuario()  void
+    //consultaUsuario()  void
+    //apagarUsuario()  void
+}
