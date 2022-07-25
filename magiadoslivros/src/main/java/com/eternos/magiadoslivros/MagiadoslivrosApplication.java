8602479fff9998777689e52ffd5244fb272d3bd2
Anderneo <anderneo.nardi@gmail.com>
2022-07-25 08:01:38 -0300

    estrutura de pastas

---
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/MagiadoslivrosApplication.java
@@ -0,0 +1,13 @@
+package com.eternos.magiadoslivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagiadoslivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagiadoslivrosApplication.class, args);
+	}
+
+}
---
+++ magiadoslivros/src/test/java/com/eternos/magiadoslivros/MagiadoslivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternos.magiadoslivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagiadoslivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
