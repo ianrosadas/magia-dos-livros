81275b2b28b9223521ebe4410b5adc597b508423
Anderneo <anderneo.nardi@gmail.com>
2022-07-25 10:10:22 -0300

    criado classe Livro

---
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/model/Livro.java
@@ -0,0 +1,20 @@
+package com.eternos.magiadoslivros.domain.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Livro {
+    private Integer idLivro;
+    private String tagEstoque;
+    private String descricao;
+    private String isbn;
+    private Double valorRecebimento;
+    private Double valorVenda;
+
+    //incluirLivro()  void
+    //atualizarLivro()  void
+    //apagarLivro()  void
+    //consultaLivro()  void
+}
