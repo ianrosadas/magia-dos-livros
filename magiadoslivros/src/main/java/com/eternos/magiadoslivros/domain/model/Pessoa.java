ca3b472cd49caa8ff65f8dcef8b09e028c401ec7
Anderneo <anderneo.nardi@gmail.com>
2022-07-26 09:35:43 -0300

    mapeado classe Pessoa

--- magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/model/Pessoa.java
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/model/Pessoa.java
@@ -1,9 +1,25 @@
 package com.eternos.magiadoslivros.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+
+@Entity
 public class Pessoa {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;  
+    @Column(name="endereco")
     protected String endereco; 
-    protected String email;  
+    @Column(name="email")
+    protected String email; 
+    @Column(name="telefone") 
     protected String telefone;  
+    @Column(name="observacao")
     protected String observacao;
 }
