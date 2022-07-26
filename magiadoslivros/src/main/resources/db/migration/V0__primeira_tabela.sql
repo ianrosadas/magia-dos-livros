488d279532a6140c01083173118bb5aabdd99663
Anderneo <anderneo.nardi@gmail.com>
2022-07-26 16:13:00 -0300

    testando nosso banco de dados com flyway

---
+++ magiadoslivros/src/main/resources/db/migration/V0__primeira_tabela.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Usuario
+(
+idUsuario INT PRIMARY KEY,
+endereco INT,
+email INT,
+telefone INT,
+rg INT,
+dataDeNascimento INT,
+gênero INT,
+dataDeCadastro INT,
+perfil INT,
+observacao INT,
+);
